1bfd92ad042c50267521f8030e9c4cbb0e3d2126
Aidan James Bennett <57372629+aidanjbennett@users.noreply.github.com>
2023-02-25 20:45:23 +0000

    add: start work on intro section

---
+++ src/components/Intro.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Warn from './Warn.vue'
+</script>
+
+<template>
+  <div class="intro">
+    <h3>Introduction</h3>
+    <p>
+      Krypton is Minecraft server software, just like Paper or Sponge, but unlike both of those,
+      it's written entirely from scratch, and doesn't depend on vanilla Minecraft.
+    </p>
+    <p>
+      Krypton's API is relatively new. Our aim is to make our API simple to understand, so it is
+      easy to learn, but powerful enough to achieve complex tasks. We also provide ways to
+      manipulate many vanilla mechanics, and many events to control what happens on the server.
+    </p>
+    <p>
+      In addition, though not directly encouraged, it is possible, and in fact, very easy, to depend
+      directly on the implementation. No longer do you have to setup Paperweight or ForgeGradle or
+      remap JARs, it is as simple as adding the server as a dependency.
+    </p>
+    <Warn text="Krypton is still in fairly early development, so go easy on us please :)" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Warn.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+  text: String
+})
+</script>
+
+<template>
+  <div class="warn">
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<style scoped>
+.warn {
+  background-color: rgba(#ff6c32, 0.3);
+  padding: 10px;
+  border-left: solid 5px #ff6c32;
+  display: block;
+}
+</style>
