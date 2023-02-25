413da53c8f02c2a94302f9d473dadce54eceba29
Aidan James Bennett <57372629+aidanjbennett@users.noreply.github.com>
2023-02-25 20:30:00 +0000

    change: remove in line style and make it into a separate class

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,10 +4,16 @@ import Hero from '../components/Hero.vue'
 </script>
 
 <template>
-  <div style="display: flex; flex-flow: column; height: 100vh">
+  <div class="container">
     <NavBar />
     <Hero />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex;
+  flex-flow: column;
+  height: 100vh;
+}
+</style>
