11b0c84aca13d6d9036bddb2338476f6ed2a9b3a
Aidan James Bennett <57372629+aidanjbennett@users.noreply.github.com>
2023-02-25 13:13:45 +0000

    add: add font awesome icons, change font size, and up padding

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,9 +5,24 @@
     <img src="../assets/images/banner.png" alt="Krypton MC Logo" class="logo" />
     <nav>
       <ul class="items">
-        <li><a href="https://github.com/KryptonMC/Krypton">Github</a></li>
-        <li><a href="https://discord.com/invite/4QuwYACDRX">Discord</a></li>
-        <li><a href="https://wiki.kryptonmc.org/">Wiki</a></li>
+        <li>
+          <a href="https://github.com/KryptonMC/Krypton">
+            <font-awesome-icon icon="fa-brands fa-github" />
+            Github
+          </a>
+        </li>
+        <li>
+          <a href="https://discord.com/invite/4QuwYACDRX">
+            <font-awesome-icon icon="fa-brands fa-discord" />
+            Discord
+          </a>
+        </li>
+        <li>
+          <a href="https://wiki.kryptonmc.org/">
+            <font-awesome-icon icon="fa-solid fa-book" />
+            Wiki
+          </a>
+        </li>
       </ul>
     </nav>
   </header>
@@ -20,8 +35,9 @@ header {
   align-items: center;
   background-color: #2f2f2f;
   height: 60px;
-  padding: 15px;
+  padding: 20px;
   font-family: 'Merriweather Sans Light', sans-serif;
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.2);
 }
 img {
   width: 200px;
@@ -32,12 +48,12 @@ nav ul li {
   list-style-type: none;
   align-items: center;
   display: inline-block;
-  font-size: 28px;
+  font-size: 24px;
 }
 
 nav ul li a {
   color: #bebebe;
-  padding: 5px 25px;
+  padding: 10px 30px;
   text-decoration: none;
 }
 
